611389d6f898a349882d9810b4d86325fad67ed1
deniska69 <gostev.dy@yandex.ru>
2025-03-18 00:36:29 +0700

    add vite ts patch plugin to up dx 👌

--- vite.config.ts
+++ vite.config.ts
@@ -1,29 +1,16 @@
-import path from 'path';
 import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
+import tsconfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
 	server: {
 		port: 5123,
 		strictPort: true,
 	},
-	plugins: [react(), tailwindcss()],
+	plugins: [react(), tailwindcss(), tsconfigPaths()],
 	base: './',
 	build: {
 		outDir: 'dist-react',
-	},
-	resolve: {
-		alias: {
-			'@assets': path.resolve(__dirname, './src/assets'),
-			'@constants': path.resolve(__dirname, './src/react/constants'),
-			'@helpers': path.resolve(__dirname, './src/react/helpers'),
-			'@hooks': path.resolve(__dirname, './src/react/hooks'),
-			'@modules': path.resolve(__dirname, './src/react/modules'),
-			'@services': path.resolve(__dirname, './src/react/services'),
-			'@stores': path.resolve(__dirname, './src/react/stores'),
-			'@types': path.resolve(__dirname, './src/react/types'),
-			'@ui': path.resolve(__dirname, './src/react/ui'),
-		},
 	},
 });
