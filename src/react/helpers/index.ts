558031e3d99ba23f3078bfb7ee6153a29f85200a
deniska69 <gostev.dy@yandex.ru>
2025-05-04 07:56:16 +0700

    add swipe gallery to preview modal 🎬

--- src/react/helpers/index.ts
+++ src/react/helpers/index.ts
@@ -13,3 +13,9 @@ export const isHide = (hide: string | boolean) => {
 export const getImageUrl = (categoryId?: string, productId?: string, imageId?: string) => {
 	return `https://city-furshet.ru/images/${categoryId}/${productId}/${imageId}.jpg`;
 };
+
+export const getGallery = (categoryId?: string, productId?: string, gallery?: string) => {
+	const arr = gallery?.replaceAll(' ', '').split(',');
+	if (!arr) return undefined;
+	return arr.map((imageId) => getImageUrl(categoryId, productId, imageId));
+};
--- src/react/modules/Forms/screens/FormsProductModalScreen.tsx
+++ src/react/modules/Forms/screens/FormsProductModalScreen.tsx
@@ -1,11 +1,24 @@
 import { MinusIcon, PlusIcon } from '@heroicons/react/24/outline';
 import { observer } from 'mobx-react';
+import { Swiper, SwiperSlide } from 'swiper/react';
 
-import { getImageUrl } from '@helpers';
+import { getGallery, getImageUrl } from '@helpers';
 import { useEscape } from '@hooks';
 import FormsHeader from '@modules/Forms/components/FormsHeader';
 import { layoutStore, priceStore } from '@stores';
-import { Button, Card, HStack, Image, Span, Stack } from '@ui';
+import { Card, Div, HStack, Image, Span, Stack } from '@ui';
+
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore
+import 'swiper/css';
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore
+import 'swiper/css/pagination';
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore
+import 'swiper/css/navigation';
+
+import { Navigation, Pagination } from 'swiper/modules';
 
 const Component = () => {
 	const handleClose = () => layoutStore.hideProductModal();
@@ -27,20 +40,55 @@ const Component = () => {
 		product_note,
 		product_price,
 		product_note_additional,
+		product_gallery,
 	} = product;
 
 	const cover = getImageUrl(categoryId, productId, product_cover);
 
+	const gallery = getGallery(categoryId, productId, product_gallery);
+
 	return (
-		<Stack className="bg-bg-dark/50 backdrop-blur-xs absolute z-[100] flex h-screen w-full items-center justify-center">
-			<Card className="!min-w-[900px] !max-w-[900px]">
+		<Stack
+			className="bg-bg-dark/50 backdrop-blur-xs absolute z-[100] flex h-screen w-full items-center justify-center"
+			onClick={handleClose}
+		>
+			<Card className="!min-w-[900px] !max-w-[900px]" onClick={(e) => e.stopPropagation()}>
 				<FormsHeader title={product_title} onClose={handleClose} />
 
 				<HStack className="mt-4 items-start relative">
-					<Image
-						src={cover}
-						className="min-w-[400px] min-h-[400px] w-[400px] aspect-square rounded-lg object-cover"
-					/>
+					{gallery ? (
+						<Div className="max-w-[400px] min-h-[400px] max-h-[400px] rounded-lg overflow-hidden">
+							<Swiper
+								pagination={true}
+								navigation={true}
+								modules={[Navigation, Pagination]}
+								className="min-h-[400px]"
+							>
+								{cover ? (
+									<SwiperSlide>
+										<Image
+											src={cover}
+											className="min-w-[400px] min-h-[400px] max-h-[400px] w-[400px] aspect-square rounded-lg object-cover"
+										/>
+									</SwiperSlide>
+								) : null}
+
+								{gallery.map((el, index) => (
+									<SwiperSlide key={index}>
+										<Image
+											src={el}
+											className="min-w-[400px] min-h-[400px] max-h-[400px] w-[400px] aspect-square rounded-lg object-cover"
+										/>
+									</SwiperSlide>
+								))}
+							</Swiper>
+						</Div>
+					) : (
+						<Image
+							src={cover}
+							className="min-w-[400px] min-h-[400px] w-[400px] aspect-square rounded-lg object-cover"
+						/>
+					)}
 
 					<Stack className="px-6 gap-y-3">
 						<Span className="text-xl font-light">{product_description}</Span>
@@ -49,17 +97,14 @@ const Component = () => {
 						</Span>
 
 						<HStack className="items-center gap-x-3">
-							<Button
-								className="mt-2 !rounded-full !px-3 hover:cursor-default"
-								variant="muted"
-							>
+							<Div className="mt-2 !rounded-full !px-3 hover:cursor-default bg-neutral-400 dark:bg-neutral-600 py-1">
 								<MinusIcon className="w-6 text-white" />
-							</Button>
+							</Div>
 							<Span className="text-2xl mt-1">0</Span>
-							<Button className="mt-2 !rounded-full !px-3 hover:cursor-default">
+							<Div className="mt-2 !rounded-full !px-3 hover:cursor-default bg-primary py-1 flex flex-row gap-x-1">
 								<Span className="!text-white" text={`${product_price || 0} ₽`} />
-								<PlusIcon className="w-6" />
-							</Button>
+								<PlusIcon className="w-6 text-white" />
+							</Div>
 						</HStack>
 
 						<Span className="text-xl font-light" variant="muted">
